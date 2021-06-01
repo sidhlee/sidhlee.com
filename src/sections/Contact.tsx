bec6abec48f323de4b63b6c7601ac9c9c4fa4179
Sid Hayoun Lee <sidhlee@gmail.com>
2021-06-01 00:52:33 -0400

    feat: add basic layout to contact section

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,11 +1,21 @@
 import styled from "styled-components"
+import Container from "../components/Container"
 
-const StyledContact = styled("div")``
+const StyledContact = styled("div")`
+  min-height: 100vh;
+  background: var(--cl-contact);
+`
 
 type ContactProps = {}
 
 const Contact: React.FC<ContactProps> = ({}) => {
-  return <StyledContact>Contact</StyledContact>
+  return (
+    <StyledContact id="contact">
+      <Container>
+        <h2>Contact</h2>
+      </Container>
+    </StyledContact>
+  )
 }
 
 export default Contact
