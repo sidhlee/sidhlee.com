1d722946c43de59c7f891fc5a8f7f5715cfef64d
Sid Hayoun Lee <sidhlee@gmail.com>
2021-06-04 03:33:05 -0400

    feat: add main section animation

--- src/sections/Main.tsx
+++ src/sections/Main.tsx
@@ -5,13 +5,24 @@ import Container from "../components/Container"
 import ButtonLink from "../components/ButtonLink"
 import SpringZoom from "../springs/SpringZoom"
 
+import { animated } from "@react-spring/web"
+import useMainChain from "../springs/useMainChain"
+
 // TODO: fix vertical scroll in main section
 const StyledMain = styled("section")`
   // Take mobile UI (keyboard/safari control bar) into account when calculating 100vh
   // https://css-tricks.com/css-fix-for-100vh-in-mobile-webkit/
   min-height: 100vh;
+  position: relative;
 
-  background-color: var(--cl-main);
+  .main-bg {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: var(--cl-main);
+  }
 
   .grid {
     /* min-height: calc(100vh - var(--py) - var(--height-navbar)); */
@@ -36,6 +47,8 @@ const StyledMain = styled("section")`
   }
 
   header {
+    position: relative;
+    z-index: 1;
     height: 100%;
     display: flex;
     flex-direction: column;
@@ -73,10 +86,45 @@ const StyledMain = styled("section")`
     }
   }
 
+  @keyframes piano {
+    0% {
+      transform: rotate(22deg) scale(1);
+    }
+    25% {
+      transform: rotate(24deg) scale(0.98);
+    }
+    50% {
+      transform: rotate(25deg) scale(0.97);
+    }
+    75% {
+      transform: rotate(26deg) scale(0.98);
+    }
+    100% {
+      transform: rotate(24deg) scale(0.99);
+    }
+  }
+  @keyframes bed {
+    0% {
+      transform: rotate(-5deg) scale(1);
+    }
+    25% {
+      transform: rotate(-3deg) scale(0.98);
+    }
+    50% {
+      transform: rotate(-4eg) scale(0.97);
+    }
+    75% {
+      transform: rotate(-7deg) scale(0.98);
+    }
+    100% {
+      transform: rotate(-6deg) scale(0.99);
+    }
+  }
+
   .piano {
     position: absolute;
     left: 50%;
-    top: -1.5vh;
+    top: -7vh;
     --size: max(300px, 40vw);
     width: var(--size);
     height: calc(var(--size) * 1.3);
@@ -84,6 +132,7 @@ const StyledMain = styled("section")`
     transform-origin: 60% 100%;
     /* prevent low contrast when piano goes under text  */
     filter: brightness(0.35);
+    /* animation: piano 7s alternate infinite linear; */
     .lid,
     .bed {
       flex-shrink: 0;
@@ -99,6 +148,7 @@ const StyledMain = styled("section")`
       margin-top: calc(-1 * 30%);
       left: 2rem;
       transform: rotate(-5deg);
+      /* animation: bed 5s alternate infinite ease; */
     }
   }
   @media (min-width: 1000px) {
@@ -111,14 +161,22 @@ const StyledMain = styled("section")`
 type MainProps = {}
 
 const Main: React.FC<MainProps> = ({}) => {
+  const {
+    h1Styles,
+    introStyles,
+    bgStyles,
+    buttonStyles,
+    pianoStyles,
+  } = useMainChain()
+
   return (
     <StyledMain id="main">
       <Container>
         <header>
-          <h1>Sid Lee</h1>
+          <animated.h1 style={h1Styles}>Sid Lee</animated.h1>
           <div className="main-content">
             <div className="main-content-inner">
-              <div className="intro">
+              <animated.div className="intro" style={introStyles}>
                 <p className="headline heading-xl">
                   Hi! I am a <span>Web Developer</span>
                 </p>
@@ -127,20 +185,26 @@ const Main: React.FC<MainProps> = ({}) => {
                   accessible and responsive web applications that offer great
                   value and experience to the user.
                 </p>
-                <SpringZoom>
-                  <ButtonLink to="/#about" $size="lg">
-                    #Open to work!
-                  </ButtonLink>
-                </SpringZoom>
-              </div>
-              <div className="piano">
-                <PianoLid className="lid" />
-                <PianoKeybed className="bed" />
-              </div>
+                <animated.div style={buttonStyles}>
+                  <SpringZoom>
+                    <ButtonLink to="/#about" $size="lg">
+                      #Open to work!
+                    </ButtonLink>
+                  </SpringZoom>
+                </animated.div>
+              </animated.div>
+
+              <animated.div style={pianoStyles}>
+                <div className="piano">
+                  <PianoLid className="lid" />
+                  <PianoKeybed className="bed" />
+                </div>
+              </animated.div>
             </div>
           </div>
         </header>
       </Container>
+      <animated.div className="main-bg" style={bgStyles}></animated.div>
     </StyledMain>
   )
 }
