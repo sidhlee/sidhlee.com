5b497f84acad6d9f910364bf5afc96004f38afb8
Sid Hayoun Lee <sidhlee@gmail.com>
2021-08-13 22:45:24 -0400

    style: darken project card overlay

--- src/features/projects/ProjectCard.tsx
+++ src/features/projects/ProjectCard.tsx
@@ -10,10 +10,10 @@ const StyledProjectCard = styled("article")`
   max-width: 380px;
   min-height: 20rem;
   position: relative;
-  box-shadow: 0 2.8px 2.2px rgba(0, 0, 0, 0.034),
-    0 6.7px 5.3px rgba(0, 0, 0, 0.048), 0 12.5px 10px rgba(0, 0, 0, 0.06),
-    0 22.3px 17.9px rgba(0, 0, 0, 0.072), 0 41.8px 33.4px rgba(0, 0, 0, 0.086),
-    0 100px 80px rgba(0, 0, 0, 0.12);
+  box-shadow: 0 2.8px 2.2px rgba(0, 0, 0, 0.04),
+    0 6.7px 5.3px rgba(0, 0, 0, 0.05), 0 12.5px 10px rgba(0, 0, 0, 0.06),
+    0 22.3px 17.9px rgba(0, 0, 0, 0.07), 0 41.8px 33.4px rgba(0, 0, 0, 0.08),
+    0 100px 80px rgba(0, 0, 0, 0.1);
 
   .overlay {
     position: absolute;
@@ -83,9 +83,9 @@ const StyledProjectCard = styled("article")`
     height: 100%;
     background: linear-gradient(
       0deg,
-      rgba(0, 0, 0, 0.8) 0%,
-      rgba(0, 0, 0, 0.6) 61%,
-      rgba(0, 0, 0, 0.8) 100%
+      rgba(0, 0, 0, 0.9) 0%,
+      rgba(0, 0, 0, 0.7) 61%,
+      rgba(0, 0, 0, 0.9) 100%
     );
     transition: opacity 250ms ease;
     opacity: 0;
